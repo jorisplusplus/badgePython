26d634a09bad06af28bd2e08afe45f1ede23b610
Joris Witteman <joriswitteman@hotmail.com>
2022-07-03 20:08:26 +0200

    Added pykeyboard config

---
+++ python_modules/pykey/testdisplay.py
@@ -0,0 +1,9 @@
+from time import sleep
+import display
+
+for i in range(0, 128):
+    display.drawFill(0)
+    for j in range(0, i):
+        display.drawPixel(j, 0, 0xFFFFFF)
+    display.flush()
+    sleep(0.4)
