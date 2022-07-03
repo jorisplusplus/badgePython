04e7b1abc27b959536b047ae8ff7a490199d04d1
Joris Witteman <joriswitteman@hotmail.com>
2022-07-03 20:05:51 +0200

    Added generic display driver based on esp-iot

---
+++ components/driver_display/driver_display.c
@@ -0,0 +1,77 @@
+#include "screen_driver.h"
+#include "driver_display.h"
+#include "buses.h"
+#include "esp_log.h"
+
+#define TAG "DISPLAY"
+
+static scr_driver_t lcd;
+static scr_info_t lcd_info;
+
+esp_err_t driver_display_set_backlight(uint8_t brightness) {
+    return ESP_OK;
+}
+
+esp_err_t driver_display_write_partial(const uint8_t *buffer, uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1) {
+    x0 = (x0/8)*8;
+    y0 = (y0/8)*8;
+    int width = x1-x0+1;
+    width = ((width + 7)/8)*8;
+    int height = y1-y0+1;
+    height = ((height + 7)/8)*8;
+    ESP_LOGW(TAG, "%d %d", width, height);
+    lcd.draw_bitmap(x0, y0, width, height, buffer);
+    return ESP_OK;
+}
+
+esp_err_t generate_interface(scr_interface_driver_t **iface_drv) {
+#ifdef CONFIG_DISPLAY_SPI
+    spi_bus_handle_t spi_bus = get_vspi_bus();
+    scr_interface_spi_config_t iface_cfg = {
+        .spi_bus = spi_bus,
+        .pin_num_cs = CONFIG_DISPLAY_PIN_CS,
+        .pin_num_dc = CONFIG_DISPLAY_PIN_DC,
+        .clk_freq = 20000000,
+        .swap_data = true,
+    };
+    return scr_interface_create(SCREEN_IFACE_SPI, &iface_cfg, iface_drv);
+#else
+    i2c_bus_handle_t i2c_bus = get_i2c0_bus();
+    scr_interface_i2c_config_t iface_cfg = {
+        .i2c_bus = i2c_bus,
+        .clk_speed = 100000,
+        .slave_addr = CONFIG_DISPLAY_I2C_ADDR
+    };
+    return scr_interface_create(SCREEN_IFACE_I2C, &iface_cfg, iface_drv);
+#endif
+}
+
+void driver_display_init() {
+    scr_interface_driver_t *iface_drv;
+    generate_interface(&iface_drv);
+    scr_controller_config_t lcd_cfg = {0};
+    lcd_cfg.interface_drv = iface_drv,
+    lcd_cfg.pin_num_rst = 0,
+    lcd_cfg.pin_num_bckl = -1,
+    lcd_cfg.rst_active_level = 0,
+    lcd_cfg.bckl_active_level = 1,
+    lcd_cfg.width = 128;
+    lcd_cfg.height = 64;
+    lcd_cfg.rotate = SCR_DIR_LRTB;
+    lcd_cfg.offset_hor = CONFIG_DISPLAY_HORIZONTAL_OFFSET;
+    lcd_cfg.offset_ver = CONFIG_DISPLAY_VERTICAL_OFFSET;
+
+    if (ESP_OK != scr_find_driver(CONFIG_DISPLAY_TYPE, &lcd)) {
+        ESP_LOGE(TAG, "Failed to find driver.");
+        return;
+    }
+    if (ESP_OK != lcd.init(&lcd_cfg)) {
+        ESP_LOGE(TAG, "Failed to init lcd.");
+        return;
+    }
+    if (ESP_OK != lcd.get_info(&lcd_info)) {
+        ESP_LOGE(TAG, "Failed to get info.");
+        return;
+    }
+    ESP_LOGI(TAG, "Screen name:%s | width:%d | height:%d", lcd_info.name, lcd_info.width, lcd_info.height);
+}
---
+++ components/driver_display/include/framebuffer_display.h
@@ -0,0 +1,22 @@
+#ifndef __FRAMEBUFFER_DISPLAY_H__
+#define __FRAMEBUFFER_DISPLAY_H__
+
+#include "driver_display.h"
+
+//This header file containes the defines used by the framebuffer driver.
+
+#define FB_SIZE (CONFIG_DISPLAY_WIDTH*CONFIG_DISPLAY_HEIGHT/8)
+#define FB_WIDTH CONFIG_DISPLAY_WIDTH
+#define FB_HEIGHT CONFIG_DISPLAY_HEIGHT
+// #ifdef CONFIG_DRIVER_ILI9341_8C
+//         #define FB_TYPE_8CBPP
+// #else
+//         #define FB_TYPE_16BPP
+// #endif
+#define FB_TYPE_1BPP
+#define FB_1BPP_VERT
+#define FB_FLUSH(buffer,eink_flags,x0,y0,x1,y1) driver_display_write_partial(buffer, x0, y0, x1, y1)
+#define FB_SET_BACKLIGHT(brightness) driver_display_set_backlight(brightness > 127)
+#define COLOR_FILL_DEFAULT 0x000000
+#define COLOR_TEXT_DEFAULT 0xFFFFFF
+#endif
