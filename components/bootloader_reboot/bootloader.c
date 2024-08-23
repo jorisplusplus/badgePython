d9a0e6be73a0af9a53daf399eb198457682c4152
Tom <mr@tomclement.nl>
2024-08-23 02:07:45 +0200

    Reverted bootloader reboot to default to normal boot again

--- components/bootloader_reboot/bootloader.c
+++ components/bootloader_reboot/bootloader.c
@@ -54,22 +54,22 @@ void reboot_into_bootloader(bool dfu) {
  */
 void bootloader_shutdown_handler(void)
 {
-//  if (next_reboot == REBOOT_BOOTLOADER) {
+  if (next_reboot == REBOOT_BOOTLOADER) {
     chip_usb_set_persist_flags(0);
     REG_WRITE(RTC_CNTL_OPTION1_REG, RTC_CNTL_FORCE_DOWNLOAD_BOOT);
 
     // stop PHY clock
     DWC2->pcgctl &= (PCGCTL_STOPPCLK | PCGCTL_GATEHCLK | PCGCTL_PWRCLMP | PCGCTL_RSTPDWNMODULE);
 
-    ets_delay_us(1000 * 1000);
-//
-//  } else if (next_reboot == REBOOT_BOOTLOADER_DFU) {
-//    chip_usb_set_persist_flags(USBDC_BOOT_DFU);
-//    REG_WRITE(RTC_CNTL_OPTION1_REG, RTC_CNTL_FORCE_DOWNLOAD_BOOT);
-//  } else {
-//    chip_usb_set_persist_flags(USBDC_PERSIST_ENA);
-//    REG_WRITE(RTC_CNTL_OPTION1_REG, 0);
-//  }
+    ets_delay_us(1 * 1000 * 1000);
+
+  } else if (next_reboot == REBOOT_BOOTLOADER_DFU) {
+    chip_usb_set_persist_flags(USBDC_BOOT_DFU);
+    REG_WRITE(RTC_CNTL_OPTION1_REG, RTC_CNTL_FORCE_DOWNLOAD_BOOT);
+  } else {
+    chip_usb_set_persist_flags(USBDC_PERSIST_ENA);
+    REG_WRITE(RTC_CNTL_OPTION1_REG, 0);
+  }
 }
 
 esp_err_t bootloader_init(void) {
